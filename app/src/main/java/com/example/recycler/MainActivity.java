da24782639caa5f718976132892d189fbff25733
Your Name <you@example.com>
2019-10-13 14:36:51 +0530

    Initial commit

---
+++ app/src/main/java/com/example/recycler/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.recycler;
+
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private List<ListData>listData;
+    private RecyclerView rv;
+    private MyAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        rv=(RecyclerView)findViewById(R.id.rev);
+        rv.setHasFixedSize(true);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+        listData=new ArrayList<>();
+
+        final DatabaseReference nm= FirebaseDatabase.getInstance().getReference("datainsert");
+        nm.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()){
+                    for (DataSnapshot npsnapshot : dataSnapshot.getChildren()){
+                        ListData l=npsnapshot.getValue(ListData.class);
+                        listData.add(l);
+                    }
+                    adapter=new MyAdapter(listData);
+                    rv.setAdapter(adapter);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+}
